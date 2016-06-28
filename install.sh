da7154b408b3bcbaafd04ff949eb12268277d8df
tsingyi <tsingyi@localhost.localdomain>
2016-06-28 15:37:59 +0800

    update install

--- install.sh
+++ install.sh
@@ -17,7 +17,12 @@ echo "installing fonts beautify ..."
 sudo cp fontconfig-infinality/* /etc/fonts/
 echo "adding repos ..."
 sudo cp yum.repos.d /etc/ -rf 
-echo "beautify grub theme"
+echo "beautify grub theme ..."
 sudo cp Vimix /boot/grub2/themes/ -rf
+
+######begin to intall essential tools
+
+echo "installing essential tools ..."
+sudo dnf install -y vim gcc gcc-c++ gnome-tweak-tool
 echo "Done! Enjoy!"
 
