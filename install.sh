26e491b3d1fb6bff2f9e5a6e7cf0e35947d593fb
hutan <hutan@localhost.localdomain>
2016-06-29 21:12:56 -0400

    update install

--- install.sh
+++ install.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-tar -xzvf ./repos.tar.gz
-tar -xzvf ./vimix.tar.gz
-tar -xzvf ./icons.tar.gz
-tar -xzvf ./theme.tar.gz
-tar -xzvf ./infinality.tar.gz
+#tar -xzvf ./repos.tar.gz
+tar -xzf ./vimix.tar.gz
+tar -xzf ./icons.tar.gz
+tar -xzf ./theme.tar.gz
+tar -xzf ./infinality.tar.gz
 
 echo "begin to install ...."
 
 echo "installing icons ..."
 mkdir ~/.icons
 cp icons ~/.icons -rf
+
 echo "installing themes ..."
 sudo cp Favorite /usr/share/themes/ -rf
+
 echo "installing fonts beautify ..."
-sudo cp fontconfig-infinality/* /etc/fonts/
-echo "adding repos ..."
-sudo cp yum.repos.d /etc/ -rf 
+sudo cp fontconfig-infinality/* /etc/fonts/ -rf 
+sudo mv /etc/fonts/conf.d/52-infinality.conf /etc/fonts/conf.d/52-infinality.conf.bak
+sudo ln -s /etc/fonts/infinality/infinality.conf /etc/fonts/conf.d/52-infinality.conf
+
+#echo "adding repos ..."
+#sudo cp yum.repos.d /etc/ -rf 
+
 echo "beautify grub theme ..."
 sudo cp Vimix /boot/grub2/themes/ -rf
+sudo echo "GRUB_THEME=/boot/grub2/themes/Vimix/theme.txt" >>/etc/default/grub
 
 ######begin to intall essential tools
 
 echo "installing essential tools ..."
-sudo dnf install -y vim gcc gcc-c++ gnome-tweak-tool
+sudo dnf config-manager --add-repo=http://repo.fdzh.org/FZUG/FZUG.repo 
+sudo dnf install fzug-release -y
+sudo dnf install -y vim gcc gcc-c++ gnome-tweak-tool powerline sogoupinyin sogoupinyin-selinux plymouth*
+echo "source /usr/share/powerline/bash/powerline.sh" >>~/.bashrc
+sudo plymouth-set-default-theme -R solar
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
 echo "Done! Enjoy!"
 
